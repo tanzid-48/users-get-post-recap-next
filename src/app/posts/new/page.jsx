d4f5da66aad2c13c9b5322fe54cba53a66bf7959
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-06 10:47:30 +0600

    fetch posts data in own server

---
+++ src/app/posts/new/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewPostsPage = () => {
+    return (
+        <div>
+           <h2>Add To New Post</h2> 
+        </div>
+    );
+};
+
+export default NewPostsPage;
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const GetPostsData = async () => {
+  const res = await fetch("http://localhost:5000/posts");
+  return res.json();
+};
+
+const PostsPage = async () => {
+  const posts = await GetPostsData();
+  return (
+    <div>
+      <h2>Post: {posts.length}</h2>
+      <div className=" flex gap-3 py-3">
+        <Link className="btn bg-yellow-500 p-1 rounded-md" href={`/`}>Home</Link>
+      <Link className="btn bg-purple-500 p-1 rounded-md" href={`/posts/new`}>Add New User</Link>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default PostsPage;
